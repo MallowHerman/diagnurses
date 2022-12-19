eaa1d7f8e70b27967257bb7aafba90cc943a0314
Herman Jorge de Melo Gonçalves <65671100+MallowHerman@users.noreply.github.com>
2022-12-19 22:34:10 -0100

    Update settings.py

--- diagnurses_project/settings.py
+++ diagnurses_project/settings.py
@@ -27,7 +27,7 @@ DEBUG = False
 
 ALLOWED_HOSTS = ['diagnurses-production.up.railway.app', '127.0.0.1']
 
-CSRF_TRUSTED_ORIGINS = ['diagnurses-production.up.railway.app']
+CSRF_TRUSTED_ORIGINS = ['https://diagnurses-production.up.railway.app']
 
 # Application definition
 
